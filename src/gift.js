d956b126a4fe83700b4877bc975473b80b7970d4
VincentLKoch <vincent.lel.koch@gmail.com>
2019-10-20 00:45:01 +0200

    depression

--- src/gift.js
+++ src/gift.js
@@ -9,11 +9,11 @@ class Gift {
 
 const giftFactory = (size) => {
     switch (size) {
-        case 'little': return new Gift(1, 0.5)
-        case 'medium': return new Gift(2, 1)
-        case 'large': return new Gift(5, 2)
-        default: return null
-    }
+        case 'little': return new Gift(1, 0.5);
+        case 'medium': return new Gift(2, 1);
+        case 'large': return new Gift(5, 2);
+        default: return null;
+    };
 }
 
 
